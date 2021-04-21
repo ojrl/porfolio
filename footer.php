7f039c2ec9e3328e7a0457caf502d8d6dd48f67e
ojrl <ojrl93>
2021-04-21 01:44:25 -0300

    21/04/2021

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer>
+    <div class="container">
+        <div class="row <?php echo $pg === "me" ? "" : "mt-5"; ?>">
+            <div class="col-12 col-sm-3 text-center text-sm-left">
+                <a href="https://github.com/ojrl" title="Githup" target="_blank"><i class="fab fa-github"></i></a>
+                <a href="https://linkedin.com" title="Linkedin" target="_blank"><i class="fab fa-linkedin"></i></a>
+            </div>
+            <div class="col-12 col-sm-3 text-center text-sm-left">
+                <a href="https://depcsuite.com/" target="_blank">DePC Suite</a>
+            </div>
+            <div class="col-12 col-sm-3 text-center text-sm-left">
+                <a href="ojrl93@gmail.com" target="_blank">ojrl93@gmail.com</a>
+            </div>
+            <div class="col-12 col-sm-3 px-2 px-sm-0 text-right">
+                <a href="https://api.whatsapp.com/send?phone=541154768187" title="Whatsapp" target="_blank"><i class="fab fa-whatsapp"></i></a>
+            </div>
+        </div>
+    </div>
+</footer>
--- proyects.php
+++ proyects.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -101,24 +101,8 @@
             </div>
         </div>
     </main>
-    <footer>
-        <div class="container">
-            <div class="row mt-5">
-                <div class="col-12 col-sm-3 text-center text-sm-left">
-                    <a href="https://github.com/ojrl" title="Githup" target="_blank"><i class="fab fa-github"></i></a>
-                    <a href="https://linkedin.com" title="Linkedin" target="_blank"><i class="fab fa-linkedin"></i></a>
-                </div>
-                <div class="col-12 col-sm-3 text-center text-sm-left">
-                    <a href="https://depcsuite.com/" target="_blank">DePC Suite</a>
-                </div>
-                <div class="col-12 col-sm-3 text-center text-sm-left">
-                    <a href="ojrl93@gmail.com" target="_blank">ojrl93@gmail.com</a>
-                </div>
-                <div class="col-12 col-sm-3 px-2 px-sm-0 text-right">
-                    <a href="https://api.whatsapp.com/send?phone=541154768187" title="Whatsapp" target="_blank"><i class="fab fa-whatsapp"></i></a>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <?php
+    include_once("footer.php"); 
+    ?>
 </body>
 </html>
